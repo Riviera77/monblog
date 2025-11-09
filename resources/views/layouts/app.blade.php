35cc586f56fcf4c49af5ee1ad8f8425bf80d0875
Grey <ingrid.freguis@gmail.com>
2025-11-09 12:37:41 +0100

    style(layout): passage en dark mode global avec polices lisibles et padding adapté

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,14 +18,15 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="font-sans antialiased">
-    <div class="min-h-screen bg-gray-100">
+<body class="bg-gray-800 text-white min-h-screen font-sans antialiased">
+    <div class="min-h-screen">
         @include('layouts.navigation')
 
         <!-- Page Heading -->
         @isset($header)
-        <header class="bg-white shadow">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <header class="bg-gray-800 shadow">
+            <div
+                class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 text-center">
                 {{ $header }}
             </div>
         </header>
