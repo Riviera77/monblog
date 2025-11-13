6d32e6e5b1e16578d7a0be91ccca199cef46ad5f
Grey <ingrid.freguis@gmail.com>
2025-11-13 19:06:17 +0100

    ajout des champs fillable + relations 'user' et 'comments' validés

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -3,14 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Article extends Model
 {
+    use HasFactory;
+
+    // Autorise les champs remplis via create([...])
+    protected $fillable = ['title', 'content', 'user_id'];
+
+    /**
+     * L’auteur de l’article
+     */
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
+    /**
+     * Commentaires validés uniquement
+     */
     public function comments()
     {
         return $this->hasMany(Comment::class)->where('is_validated', true);
