eca194403b53aa9d9888eb1fdcceb4e19938c2dd
Grey <ingrid.freguis@gmail.com>
2025-11-24 20:18:09 +0100

    appel asynchrone avec fetch()

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,9 @@
                 Ici, je partage mes apprentissages sur le web et
                 l’indépendance des femmes de 40+.</p>
         </div>
+        <div id="flash-message"
+            class="text-2xl font-semibold tracking-tight text-pretty text-purple-400 sm:text-2xl">
+        </div>
         <div
             class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-700 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
             @forelse ($articles as $article)
